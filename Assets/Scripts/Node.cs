827bd4ae311ae953588591ba93f770fa788bd00f
BugStory101 <131591574+BugStory101@users.noreply.github.com>
2023-08-16 16:10:21 +0900

    타일맵
    타일맵 업데이트

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Node
+{
+    public int G;
+    public int H;
+    public int F
+    {
+        get
+        {
+            return G + H;
+        }
+    }
+
+    public Vector3Int Position;
+    public Node ParentNode { get; set; }
+
+    public Node(Vector3Int position)
+    {
+        Position = position;
+    }
+}
